7a7bee451296164dbd4d0e13ae674608348ecfe7
Dairu Fatahillah <dairufatahillah60@gmail.com>
2026-03-05 17:37:05 +0700

    Praktikum 3.4

--- MatakuliahDemo.java
+++ MatakuliahDemo.java
@@ -23,6 +23,13 @@ public class MatakuliahDemo {
 
             arrayOfMatakuliah[i] = new Matakuliah(kode, nama, sks, jumlahJam);
         }
-        
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            System.out.println("Kode       : " + arrayOfMatakuliah[i].kode);
+            System.out.println("Nama       : " + arrayOfMatakuliah[i].nama);
+            System.out.println("Sks        : " + arrayOfMatakuliah[i].sks);
+            System.out.println("Jumlah Jam : " + arrayOfMatakuliah[i].jumlahJam);
+            System.out.println("--------------------------------------");
+        }
     }
 }
